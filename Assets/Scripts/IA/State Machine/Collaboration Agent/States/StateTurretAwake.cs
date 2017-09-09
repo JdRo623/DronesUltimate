286755cbf2141ea422451f99713c26021b19afcf
JdRo623 <JdRodriguez623@outlook.com>
2017-09-09 10:10:56 -0500

    IA

---
+++ Assets/Scripts/IA/State Machine/Collaboration Agent/States/StateTurretAwake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateTurretAwake : State {
+    public override void OnEntryAction()
+    {
+
+    }
+    public override void OnUpdateAction()
+    {
+
+    }
+    public override void OnExitAction()
+    {
+    }
+}
---
+++ Assets/Scripts/IA/State Machine/Collaboration Agent/States/StateTurretDeath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateTurretDeath : State
+{
+    public override void OnEntryAction(){
+
+    }
+    public override void OnUpdateAction(){
+
+    }
+    public override void OnExitAction(){
+    }
+}
